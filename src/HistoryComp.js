2aa4b42635538f66bf768424b54c310f57e377be
Abdul Muqeet <muqeeabd@corp.contoursoftware.net>
2023-01-27 22:31:44 +0500

    added functionality of delete and alert 27-jan-2023

--- src/HistoryComp.js
+++ src/HistoryComp.js
@@ -1,60 +1,159 @@
-import React, { useEffect, useState } from 'react';
-import CopyToClipboard from 'react-copy-to-clipboard';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { Table } from 'react-bootstrap';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import CopyToClipboard from "react-copy-to-clipboard";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Table, Button, Modal, Form } from "react-bootstrap";
+import "./App.css";
+import { MdDelete, MdOutlineFileCopy } from "react-icons/md";
+import { AiOutlineEdit } from "react-icons/ai";
 
 const HistoryComp = () => {
+  const [localData, setLocalData] = useState([]);
+  const [deleteId, setdeleteId] = useState("");
+  const [copied, setcopied] = useState(false);
+  const [showdelete, setshowdelete] = useState(false);
+  const [showupdate, setshowupdate] = useState(false);
 
-    const [localData, setLocalData] = useState([]);
-    const [copied , setcopied] = useState(false);
+  
+  useEffect(() => {
+    const items = JSON?.parse(localStorage?.getItem('newShortURL'));
+    setLocalData(items);
+  }, []);
 
-    useEffect(() => {
-        const items = JSON.parse(localStorage.getItem('newShortURL'));
-        setLocalData(items);
-    }, []);
+  const handleClose = () => {
+    setshowdelete(false);
+    setshowupdate(false);
+    
+  };
 
-    return (
-        <div className='showurl'>
+  const handleDeleteItem = () => {
+    const filtered = localData.filter((filter) => {
+      return filter?.id !== deleteId;
+    });
+    // localStorage.removeItem(deleteId)
+    localStorage?.setItem('newShortURL', JSON?.stringify(filtered));
+    setLocalData(filtered);
+    setshowdelete(false);
+  };
 
-            {/* Table to show data */}
-            <Table striped bordered  className="tabledata">
-                <thead>
-                    <tr>
-                        <th>#</th>
-                        <th>Long-URL</th>
-                        <th>Short-URL</th>
-                        <th>Expiry Date</th>
-                        <th>Copy</th>
-                        <th>Edit</th>
-                    </tr>
-                </thead>
+  const handleDeleteModal = (id) => {
+    setdeleteId(id);
+    setshowdelete(true);
+  };
 
-                <tbody>
-                    {/* map fucntion to show data in required fileds */}
-                    {localData.map((item, i) => (
-                        <tr key={i}>
-                            <td>{i+1}</td>
-                            <td>{item.Url}</td>
-                            <td>{item.shortUrl}</td>
-                            <td>{item.date}</td>
-                            <td>{
-                                    <CopyToClipboard 
-                                        text={item.shortUrl}
-                                        onCopy={() => setcopied(true)}
-                                    >   
-                                     <button className={copied ? "copied" : ""}>copy</button>
-                                    </CopyToClipboard>
-                                }
-                            </td>
-                            <td> </td>
-                        </tr>
-                    ))}
-                </tbody>
-            </Table>
+  const handleUpdate = (updateid) => {
+    setshowupdate(true);
+    
+  }
+  
+  const handleUpdateSubmit = () => {
 
-        </div>
-    )
-}
+  }
+
+  return (
+    <div className="showurl">
+
+      {/* Delete Modal Section */}
+      <Modal show={showdelete} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Delete Item</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>Are you sure? You want to delete!</Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+          <Button variant="danger" onClick={handleDeleteItem}>
+            Delete
+          </Button>
+        </Modal.Footer>
+      </Modal>
+
+      {/* Update Modal */}
+      {/* Delete Modal Section */}
+      <Modal show={showupdate} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Update</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Form onSubmit={handleUpdateSubmit} >
+            <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+              <Form.Label>Enter New URL</Form.Label>
+              <Form.Control
+                type="text"
+                autoFocus
+              />
+            </Form.Group>
+            <Form.Group
+              className="mb-3"
+              controlId="exampleForm.ControlTextarea1"
+            >
+              <Form.Label>New Expiry Date</Form.Label>
+              <Form.Control type="date"/>
+            </Form.Group>
+          </Form>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+          <Button variant="primary" onClick={handleClose}>
+            Save Changes
+          </Button>
+        </Modal.Footer>
+      </Modal>
+      {/* Table to show data */}
+      <Table striped bordered className="tabledata">
+        <thead>
+          <tr>
+            <th>#</th>
+            <th>Long-URL</th>
+            <th>Short-URL</th>
+            <th>Expiry Date</th>
+            <th>Copy</th>
+            <th>Edit</th>
+          </tr>
+        </thead>
+
+        <tbody>
+          {/* map fucntion to show data in required fileds */}
+          {localData?.map((item, i) => (
+            <tr key={item?.id}>
+              <td>{i + 1}</td>
+              <td>{item?.Url}</td>
+              <td>
+                <a href={item?.Url} target={"_blank"}>
+                  {item?.newshortUrl}
+                </a>
+              </td>
+              <td>{item?.date}</td>
+              <td>
+                {
+                  <CopyToClipboard
+                    text={item?.shortUrl}
+                    onCopy={() => setcopied(true)}
+                  >
+                    <button>
+                      <MdOutlineFileCopy />
+                    </button>
+                  </CopyToClipboard>
+                }
+              </td>
+              <td>
+                {/* Delete Icon in edit section */}
+                <button onClick={() => handleDeleteModal(item?.id)}>
+                  <MdDelete />
+                </button>
+                {/* Update Icon in edit section */}
+                <button onClick={() => handleUpdate(item?.id)} >
+                  <AiOutlineEdit />
+                </button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </Table>
+    </div>
+  );
+};
 
 export default HistoryComp;
--- src/Homepage.js
+++ src/Homepage.js
@@ -1,51 +1,96 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import "./App.css";
-import shortid from 'shortid';
+import "bootstrap/dist/css/bootstrap.min.css";
+import Alert from 'react-bootstrap/Alert';
+import shortid from "shortid";
 
 const Homepage = () => {
+  const [Url, setUrl] = useState([]);
+  const [date, setdate] = useState("");
+  const [arr, setArr] = useState([]);
+  const [shorten, setshorten] = useState(true);
+  const [show, setShow] = useState(false);
 
-    const [Url, setUrl] = useState("");
-    const [date, setdate] = useState("");
-    const [shortUrl, setshortUrl] = useState([]);
-    const [arr, setArr] = useState([]);
+  const handlesubmit = (e) => {
+    e.preventDefault();
 
-    const handlesubmit = (e) => {
-        e.preventDefault()
-        const newShortURL = shortid.generate();
-        setshortUrl(newShortURL);
+    const regExp = RegExp('^(https?:\\/\\/)?'+ // protocol
+    '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|'+ // domain name
+    '((\\d{1,3}\\.){3}\\d{1,3}))'+ // OR ip (v4) address
+    '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*'+ // port and path
+    '(\\?[;&a-z\\d%_.~+=-]*)?'+ // query string
+    '(\\#[-a-z\\d_]*)?$','i');
 
-        const urlData = {
-            shortUrl, Url, date
-        }
-
-        setArr((current) => [...current, urlData]);
-
-        localStorage.setItem("newShortURL", JSON.stringify(arr));
+    if (!Url.match(regExp)) {
+      setShow(true);
     }
+    else{
+    
+      const shortId = shortid.generate();
+      const newshortUrl = `${window.location.href}${shortId}`;
 
+      const urlData = {
+        id: shortId,
+        Url,
+        newshortUrl,
+        date,
+      };
 
-    return (
-        <div className='inputcontainer'>
-            <h1>URL-<span>Shortner</span></h1>
-            <div>
-                <form onSubmit={handlesubmit}>
-                    <input
-                        type="text"
-                        placeholder='Please Enter URL...'
-                        value={Url}
-                        onChange={(e) => setUrl(e.target.value)}
-                    />
+      localStorage.setItem("newShortURL", JSON.stringify([...arr, urlData]));
+      setArr([...arr, urlData]);
+      setShow(false);
+    }
+  };
 
-                    <input
-                        className="inputdate"
-                        type="date"
-                        onChange={(e) => setdate(e.target.value)}
-                    />
-                    <button type='submit' >Shorten!</button>
-                </form>
-            </div>
-        </div>
-    )
-}
+  const handleURLinput = (e) => {
+
+    if(e.target.value.length > 0){
+      setUrl(e.target.value);
+      setshorten(false);
+    }
+    else{
+      setshorten(true);
+    }
+  }
+
+  return (
+    <div className="inputcontainer">
+    
+      
+      <Alert variant="danger" onClose={() => setShow(false)} dismissible>
+        <Alert.Heading>Oh snap! You got an error!</Alert.Heading>
+        <p>
+          You have entered an invalid URL
+        </p>
+      </Alert>
+
+      <h1>
+        URL-<span>Shortner</span>
+      </h1>
+      <div>
+        <form onSubmit={handlesubmit}>
+          <input
+            type="text"
+            placeholder="Please Enter URL..."
+            defaultValue={Url}
+            onChange={(e) => handleURLinput(e)}
+          />
+
+          <input
+            className="inputdate"
+            type="date"
+            onChange={(e) => setdate(e.target.value)}
+          />
+          <button type="submit" 
+            onClick={() => setShow(true)}
+            className="btnshorten"
+            disabled={shorten}>
+              Shorten!
+            </button>
+        </form>
+      </div>
+    </div>
+  );
+};
 
 export default Homepage;
